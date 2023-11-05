cf5061083fcbe1913f7f6be1a2ccc74d3e8b6f81
Hugo Kapp <hugo.kapp6@hotmail.fr>
2023-11-05 10:21:13 +0100

    Get the parl kmeans test to run
    They don't all pass yet

--- src/kmeans/seq.rs
+++ src/kmeans/seq.rs
@@ -249,28 +249,8 @@ impl<T> Clusters<T> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use super::super::test::square_centered_at;
     use std::collections::HashSet;
-
-    impl Point for (i32, i32) {
-        fn dist(&self, other: &Self) -> f64 {
-            fn diff_squared(a: i32, b: i32) -> f64 {
-                ((a - b) as f64).powi(2)
-            }
-
-            (diff_squared(self.0, other.0) + diff_squared(self.1, other.1)).sqrt()
-        }
-
-        fn mean(points: &[Self]) -> Self {
-            let sum = points.iter()
-                        .map(|(a, b)| (*a as i64, *b as i64))
-                        .fold((0, 0), |(r, u), (a, b)| (r + a, u + b));
-
-            let div = |x: i64| -> i32 {
-                (x / points.len() as i64) as i32
-            };
-            (div(sum.0), div(sum.1))
-        }
-    }
 
     fn assert_centroids<T>(clusters: &Clusters<T>, expected_centroids: &[T])
         where T: Clone + Eq + std::hash::Hash + std::fmt::Debug
@@ -293,20 +273,6 @@ mod test {
         for i in 0..data.len() {
             assert_eq!(clusters.assignment[i], vec![clusters.centroids[i]]);
         }
-    }
-
-    /* . . .         * * *
-     * . * .    =>   * * *
-     * . . .         * * *
-     */
-    fn square_centered_at(p: (i32, i32)) -> Vec<(i32, i32)> {
-        let mut square = Vec::new();
-        for i in -1..2 {
-            for j in -1..2 {
-                square.push((p.0 + i, p.1 + j));
-            }
-        }
-        square
     }
 
     #[test]
@@ -334,30 +300,6 @@ mod test {
         let clusters = super::cluster(&mut data.into_iter(), 2);
         println!("{:?}", &clusters);
         assert_centroids(&clusters, &sq_centers[..]);
-    }
-
-    #[test]
-    fn dist1() {
-        assert_eq!((0, 0).dist(&(0, 1)), 1.0);
-    }
-
-    #[test]
-    fn dist2() {
-        let sq_center = (-100, 0);
-        let correct_centroid = (-11, 0);
-        let wrong_centroid = (11, 0);
-
-        for p in square_centered_at(sq_center) {
-            let closer = correct_centroid.dist(&p);
-            let further = wrong_centroid.dist(&p);
-            assert!(closer < further);
-        }
-    }
-
-    #[test]
-    fn mean1() {
-        let sq_center = (-100, 0);
-        assert_eq!(Point::mean(&square_centered_at(sq_center)), sq_center);
     }
 
     #[test]
