e6b7ef49ef8a9993abe45c3cc695f8094293f0e7
Hugo Kapp <hugo.kapp6@hotmail.fr>
2024-06-12 08:07:35 +0200

    Generate the diagrams with the zip-dict encoder

--- boxplot.py
+++ boxplot.py
@@ -3,6 +3,7 @@ import os
 import glob
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 csv_folder = 'output/'
 
@@ -13,19 +14,24 @@ data = []
 
 for csv_path in glob.glob(csv_folder + "*.csv"):
     csv_data = pd.read_csv(csv_path)
-    data.append(csv_data["compression_ratio"])
+    # Only keep the lossless codecs
+    if not "error" in csv_data or csv_data["error"].mean() == 0:
+        data.append(csv_data["compression_ratio"])
 
-    # Go from 'dir/filename.csv' to 'filename'
-    file_name = os.path.split(csv_path)[1]
-    display_name = os.path.splitext(file_name)[0]
-    names.append(display_name)
+        # Go from 'dir/filename.csv' to 'filename'
+        file_name = os.path.split(csv_path)[1]
+        display_name = os.path.splitext(file_name)[0]
+        names.append(display_name)
 
 # Generate the plot
 
+ax = plt.gca()
+
 plt.ylabel('Compression ratio (lower is better)')
 plt.ylim(0, 100)
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:g}'.format(y)+'%'))
 
-plt.gca().set_xticklabels(names)
+ax.set_xticklabels(names)
 
 plt.boxplot(data, showmeans=True)
 plt.savefig("output/boxplot.png")
