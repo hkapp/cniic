dfe7f1da412d2d86e52d8346ae13f28e33ff9d20
Hugo Kapp <hugo.kapp6@hotmail.fr>
2024-06-18 22:44:54 +0200

    Add much more comments in prs.rs

--- src/prs.rs
+++ src/prs.rs
@@ -14,11 +14,12 @@ pub fn matches_fully<'a>(input: &'a str, regex: &str) -> Option<&'a str> {
     }
 }
 
+/// Parse a function call of the form `<fun-name>([<arg>,]...)`
 pub fn fun_call(input: &str) -> Option<(&str, Vec<&str>)> {
     let mut fun_name = None;
     let mut params = Vec::new();
-    let mut paren_stack = 0;
-    let mut last_pos = 0;
+    let mut paren_stack: u16 = 0;
+    let mut last_pos: usize = 0;
 
     for (new_pos, c) in input.chars()
                                 .enumerate()
@@ -27,45 +28,72 @@ pub fn fun_call(input: &str) -> Option<(&str, Vec<&str>)> {
     {
         match c {
             '(' => {
-                if paren_stack > 0 {
-                    // We are already parsing the arguments
-                    // We will now ignore everything until the correct closing paren
-                    paren_stack += 1;
-                    // Important: don't update last_pos here
-                }
-                else {
-                    // First opening parenthesis
-                    // Keep track of the fun_name
+                if paren_stack == 0 {
                     if fun_name.is_some() || last_pos != 0 {
+                        // Invalid input:
+                        // a()(b, c)
+                        //    ^
                         return None;
                     }
                     assert!(params.len() == 0);
                     if new_pos == 0 {
+                        // Invalid input:
+                        // (b, c)
+                        // ^
                         return None;
                     }
+
+                    // The input is valid:
+                    // a(b, c)
+                    //  ^
+                    // This is the first opening parenthesis
+                    // Keep track of the fun_name
                     fun_name = Some(&input[0..new_pos]);
                     paren_stack = 1;
                     last_pos = new_pos + 1;  // ignore the parenthesis itself
                 }
+                else {
+                    // We are already parsing the arguments
+                    // a(b, c(d, e))
+                    //       ^
+                    // We now ignore everything until the corresponding closing paren
+                    paren_stack += 1;
+                    // Important: don't update last_pos here
+                }
             }
             ')' => {
-                if paren_stack <= 0 {
+                if paren_stack == 0 {
+                    // Invalid input:
+                    // a())
+                    //    ^
                     return None;
                 }
                 if paren_stack == 1 {
                     if new_pos != (input.len() - 1) {
+                        // Invalid input:
+                        // a(b, c)d
+                        //       ^
                         return None;
                     }
                     else {
                         // This is the last argument
                         assert!(fun_name.is_some());
-                        // Note: here we accept a 0-length argument to allow inputs of the form "a()"
+
                         if new_pos == last_pos {
                             if params.len() > 0 {
+                                // Invalid input:
+                                // a(b, c,)
+                                //        ^
                                 return None;
                             }
+                            // else: valid input
+                            // a()
+                            //   ^
                         }
                         else {
+                            // Valid input:
+                            // a(b, c)
+                            //       ^
                             params.push(&input[last_pos..new_pos]);
                         }
                         last_pos = new_pos + 1;
@@ -77,21 +105,33 @@ pub fn fun_call(input: &str) -> Option<(&str, Vec<&str>)> {
             ',' => {
                 match paren_stack {
                     0 => {
-                        // Invalid input
+                        // Invalid input:
+                        // a,b
+                        //  ^
                         return None;
                     }
                     1 => {
                         // We now have an argument
                         assert!(fun_name.is_some());
+
                         if last_pos == new_pos {
+                            // Invalid input:
+                            // a(b,,c)
+                            //     ^
                             return None;
                         }
+
+                        // Valid input:
+                        // a(b, c)
+                        //    ^
                         params.push(&input[last_pos..new_pos]);
                         last_pos = new_pos + 1;
                     }
                     _ => {
                         assert!(paren_stack > 1);
-                        // Nothing to do here
+                        // Nothing to do here:
+                        // a(b, c(d, e))
+                        //         ^
                     }
                 }
             }
@@ -100,6 +140,9 @@ pub fn fun_call(input: &str) -> Option<(&str, Vec<&str>)> {
     }
 
     if paren_stack != 0 {
+        // Invalid input:
+        // a(
+        //  ^
         return None;
     }
     else {
