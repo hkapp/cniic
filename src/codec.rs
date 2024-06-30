42243738b03f8649803fc705acd1c293d11ebf0f
Hugo Kapp <hugo.kapp6@hotmail.fr>
2024-06-30 15:12:33 +0200

    Generate the entire file codec.rs using macros

--- src/codec.rs
+++ src/codec.rs
@@ -14,103 +14,107 @@ pub trait Codec {
     fn is_lossless(&self) -> bool;
 }
 
-pub enum AnyCodec {
-    Hufman(hufc::Hufman),
-    ClusterColors(clusterc::ClusterColors),
-    VoronoiCluster(clusterc::VoronoiCluster),
-    Zip(zipc::Zip),
+// To add new codecs, simply add an entry in the gen_all
+// macro invocation at the bottom of this file
+
+macro_rules! gen_struct {
+    ( $( $mod:ident, $codec:ident );* ) => {
+        pub enum AnyCodec {
+            $(
+                $codec($mod::$codec),
+            )*
+        }
+    };
 }
 
 type CodecFromStrErr = Vec<(String, String)>;
 
-// TODO introduce a macro for generating all of this boiler-plate code
-impl FromStr for AnyCodec {
-    type Err = CodecFromStrErr;
+macro_rules! gen_from_str_impl {
+    ( $( $mod:ident, $codec:ident );* ) => {
+        impl FromStr for AnyCodec {
+            type Err = CodecFromStrErr;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        fn stack_err(mut prev_err: CodecFromStrErr, name: &str, new_err: String) -> CodecFromStrErr {
-            prev_err.push((String::from(name), new_err));
-            prev_err
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                fn stack_err(mut prev_err: CodecFromStrErr, name: &str, new_err: String) -> CodecFromStrErr {
+                    prev_err.push((String::from(name), new_err));
+                    prev_err
+                }
+
+                Err(Vec::new())
+                $(
+                    .or_else(|prev_err: CodecFromStrErr| {
+                        $mod::$codec::from_str(s)
+                            .map(Into::into)
+                            .map_err(|new_err| stack_err(prev_err, stringify!($codec), new_err))
+                    })
+                )*
+            }
         }
-
-        Err(Vec::new())
-            .or_else(|prev_err: CodecFromStrErr|
-                hufc::Hufman::from_str(s)
-                    .map(Into::into)
-                    .map_err(|new_err| stack_err(prev_err, "Hufman", new_err)))
-            .or_else(|prev_err|
-                clusterc::ClusterColors::from_str(s)
-                    .map(Into::into)
-                    .map_err(|new_err| stack_err(prev_err, "ClusterColors", new_err)))
-            .or_else(|prev_err|
-                clusterc::VoronoiCluster::from_str(s)
-                    .map(Into::into)
-                    .map_err(|new_err| stack_err(prev_err, "VoronoiCluster", new_err)))
-            .or_else(|prev_err|
-                zipc::Zip::from_str(s)
-                    .map(Into::into)
-                    .map_err(|new_err| stack_err(prev_err, "Zip", new_err)))
-    }
+    };
 }
 
-impl Codec for AnyCodec {
-    fn encode<W: io::Write>(&self, img: &Img, writer: &mut W) -> io::Result<()> {
-        match self {
-            AnyCodec::Hufman(h)  => h.encode(img, writer),
-            AnyCodec::ClusterColors(c) => c.encode(img, writer),
-            AnyCodec::VoronoiCluster(c) => c.encode(img, writer),
-            AnyCodec::Zip(c) => c.encode(img, writer),
+macro_rules! gen_codec_impl {
+    ( $( $codec:ident );* ) => {
+        impl Codec for AnyCodec {
+            fn encode<W: io::Write>(&self, img: &Img, writer: &mut W) -> io::Result<()> {
+                match self {
+                    $(
+                        AnyCodec::$codec(c) => c.encode(img, writer),
+                    )*
+                }
+            }
+
+            fn decode<I: Iterator<Item = u8>>(&self, reader: &mut I) -> Option<Img> {
+                match self {
+                    $(
+                        AnyCodec::$codec(c) => c.decode(reader),
+                    )*
+                }
+            }
+
+            fn name(&self) -> String {
+                match self {
+                    $(
+                        AnyCodec::$codec(c) => c.name(),
+                    )*
+                }
+            }
+
+            fn is_lossless(&self) -> bool {
+                match self {
+                    $(
+                        AnyCodec::$codec(c) => c.is_lossless(),
+                    )*
+                }
+            }
         }
-    }
-
-    fn decode<I: Iterator<Item = u8>>(&self, reader: &mut I) -> Option<Img> {
-        match self {
-            AnyCodec::Hufman(h)  => h.decode(reader),
-            AnyCodec::ClusterColors(c) => c.decode(reader),
-            AnyCodec::VoronoiCluster(c) => c.decode(reader),
-            AnyCodec::Zip(c) => c.decode(reader),
-        }
-    }
-
-    fn name(&self) -> String {
-        match self {
-            AnyCodec::Hufman(h)  => h.name(),
-            AnyCodec::ClusterColors(c) => c.name(),
-            AnyCodec::VoronoiCluster(c) => c.name(),
-            AnyCodec::Zip(c) => c.name(),
-        }
-    }
-
-    fn is_lossless(&self) -> bool {
-        match self {
-            AnyCodec::Hufman(h)  => h.is_lossless(),
-            AnyCodec::ClusterColors(c) => c.is_lossless(),
-            AnyCodec::VoronoiCluster(c) => c.is_lossless(),
-            AnyCodec::Zip(c) => c.is_lossless(),
-        }
-    }
+    };
 }
 
-impl From<hufc::Hufman> for AnyCodec {
-    fn from(h: hufc::Hufman) -> Self {
-        AnyCodec::Hufman(h)
-    }
+macro_rules! gen_from_impl {
+    ( $( $mod:ident, $codec:ident );* ) => {
+        $(
+            impl From<$mod::$codec> for AnyCodec {
+                fn from(h: $mod::$codec) -> Self {
+                    AnyCodec::$codec(h)
+                }
+            }
+        )*
+    };
 }
 
-impl From<clusterc::ClusterColors> for AnyCodec {
-    fn from(c: clusterc::ClusterColors) -> Self {
-        AnyCodec::ClusterColors(c)
-    }
+macro_rules! gen_all {
+    ( $( $mod:ident, $codec:ident );* ) => {
+        gen_struct!($($mod, $codec);*);
+        gen_from_str_impl!($($mod, $codec);*);
+        gen_codec_impl!($($codec);*);
+        gen_from_impl!($($mod, $codec);*);
+    };
 }
 
-impl From<clusterc::VoronoiCluster> for AnyCodec {
-    fn from(c: clusterc::VoronoiCluster) -> Self {
-        AnyCodec::VoronoiCluster(c)
-    }
-}
-
-impl From<zipc::Zip> for AnyCodec {
-    fn from(c: zipc::Zip) -> Self {
-        AnyCodec::Zip(c)
-    }
-}
+gen_all!(
+    hufc, Hufman;
+    clusterc, ClusterColors;
+    clusterc, VoronoiCluster;
+    zipc, Zip
+);
